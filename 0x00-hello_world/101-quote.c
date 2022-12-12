0bd9a5afc12f696da397b5bd92ceac8576439026
Kayah2000 <isiakarukayat10@gmail.com>
2022-12-12 13:37:43 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
 
 
 /**
- * main - prints exactly and that piece of art is useful
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 1
  */
 
 int main(void)
